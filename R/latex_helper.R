bef8faa12e72f372114a51e3093d939cd7e66e76
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-09-27 17:22:20 +0700

    2nd commit

---
+++ R/latex_helper.R
@@ -0,0 +1,33 @@
+### Latex Helper
+
+
+
+# Bold --------------------------------------------------------------------
+
+
+latex_bold <- function(text) {
+  
+  glue::glue("\\textbf{<text>}",
+             .open = "<", .close = ">")
+}
+
+
+# Color -------------------------------------------------------------------
+
+
+latex_color <- function(text, color = "red") {
+  
+  glue::glue("\\textcolor{<color>}{<text>}",
+             .open = "<", .close = ">")
+}
+
+
+# Hyperlink ---------------------------------------------------------------
+
+
+latex_hyperlink <- function(text, url, color = "blue") {
+  
+  glue::glue("\\href{<url>}{\\textcolor{<color>}{<text>}}",
+             .open = "<", .close = ">")
+  
+}
